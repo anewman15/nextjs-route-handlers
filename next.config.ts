b1e508a90a89ff6d7be5c9959ccf1b48e54fdb4d
Abdullah Numan <anewman15@hotmail.com>
2025-08-31 11:28:32 +0600

    feat: enable ppr

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    ppr: "incremental",
+  },
+  images: {
+    domains: ["github.com"], // Add your allowed domains here
+  },
 };
 
 export default nextConfig;
