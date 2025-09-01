e24dd9a4ed782d395d03cdddb510c1ba4650a8c9
Abdullah Numan <anewman15@hotmail.com>
2025-09-01 13:39:45 +0600

    feat: add invoices buttons

---
+++ app/ui/invoices/buttons.tsx
@@ -0,0 +1,66 @@
+import {
+  DocumentArrowDownIcon,
+  DocumentCheckIcon,
+  PencilIcon,
+  PlusIcon,
+  TrashIcon,
+} from '@heroicons/react/24/outline';
+import Link from 'next/link';
+
+export function CreateInvoice() {
+  return (
+    <Link
+      href="/dashboard/invoices/create"
+      className="flex h-10 items-center rounded-lg bg-blue-600 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+    >
+      <span className="hidden md:block">Create&nbsp;Invoice</span>&nbsp;&nbsp;
+      <PlusIcon className="h-5 md:ml-4" />
+    </Link>
+  );
+};
+
+export function DownloadCSV() {
+  return (
+    <Link
+      href={`/api/v1/invoices/csv`}
+      className="h-10 px-4 flex items-center rounded-lg bg-blue-600 text-sm font-medium text-white transition-colors hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+    >
+      <span className="hidden md:block text-white">Download&nbsp;as&nbsp;CSV</span>&nbsp;&nbsp;
+      <DocumentArrowDownIcon className="h-5" />
+    </Link>
+  );
+};
+
+export function ViewInvoice({ id }: { id: string }) {
+  return (
+    <Link
+      href={`/dashboard/invoices/${id}`}
+      className="rounded-md border p-2 hover:bg-gray-100"
+    >
+      <DocumentCheckIcon className="w-5" />
+    </Link>
+  );
+};
+
+export function UpdateInvoice({ id }: { id: string }) {
+  return (
+    <Link
+      href={`/dashboard/invoices/${id}/edit`}
+      className="rounded-md border p-2 hover:bg-gray-100"
+    >
+      <PencilIcon className="w-5" />
+    </Link>
+  );
+};
+
+export function DeleteInvoice({ id }: { id: string }) {
+
+  return (
+    <form>
+      <button type="submit" className="rounded-md border p-2 bg-gray-400 hover:bg-red-500">
+        <span className="sr-only">Delete</span>
+        <TrashIcon className="w-5 stroke-gray-50" />
+      </button>
+    </form>
+  );
+};
