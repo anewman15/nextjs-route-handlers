2fb3bea7d65e3a9ef6604332fe4915ffab0acfc4
Abdullah Numan <anewman15@hotmail.com>
2025-09-03 12:17:14 +0600

    feat: add api route at api/v2/invoices/(locales)/en-us/[id]

---
+++ app/api/v2/invoices/(locales)/en-us/[id]/route.ts
@@ -0,0 +1,48 @@
+import { NextRequest, NextResponse } from "next/server";
+import { invoices } from "@/app/lib/strapi/strapiClient";
+
+export async function GET(
+  request: NextRequest,
+  { params }: { params: Promise<{ id: string }>}
+) {
+
+  const id = (await params).id;
+
+  try {
+    const invoice = await invoices.findOne(id);
+    return NextResponse.json(invoice);
+  } catch (e) {
+    return NextResponse.json(e);
+  };
+};
+
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: Promise<{ id: string }> }
+) {
+
+  const id = (await params).id;
+  const formData = await request.json();
+
+  try {
+    const invoice = await invoices.update(id, formData);
+    return NextResponse.json(invoice);
+  } catch (e) {
+    return NextResponse.json(e);
+  };
+};
+
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: Promise<{ id: string }> }
+) {
+
+  const id = (await params).id;
+
+  try {
+    const invoice = await invoices.delete(id);
+    return NextResponse.json(invoice);
+  } catch (e) {
+    return NextResponse.json(e);
+  };
+};
