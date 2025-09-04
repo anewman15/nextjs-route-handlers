8af12ee51482188d2042c88ba991b5630b3fd541
Abdullah Numan <anewman15@hotmail.com>
2025-09-04 15:27:31 +0600

    feat: add api route at /api/v2/revenues/(locales)/en-us

---
+++ app/api/v2/revenues/(locales)/en-us/route.ts
@@ -0,0 +1,11 @@
+import { NextResponse } from "next/server";
+import { revenues } from "@/app/lib/strapi/strapiClient";
+
+export async function GET() {
+  try {
+    const allRevenues = await revenues.find();
+    return NextResponse.json(allRevenues);
+  } catch (e) {
+    return NextResponse.json(e);
+  }
+};
