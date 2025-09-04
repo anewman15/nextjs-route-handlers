a3e785124d757aaf02fea23029114935d133159b
Abdullah Numan <anewman15@hotmail.com>
2025-09-04 15:28:09 +0600

    feat: add redirects management middleware

---
+++ middleware.ts
@@ -0,0 +1,36 @@
+import { NextRequest, NextResponse } from "next/server";
+import { has as edgeConfigHas } from "@vercel/edge-config";
+
+export interface RedirectsEntry {
+  destination: string;
+  permanent: boolean;
+}
+
+export type RedirectsRecord = Record<string, RedirectsEntry>;
+
+export async function middleware(request: NextRequest) {
+  const pathname = request?.nextUrl?.pathname;
+  const redirectKey = pathname?.split("/")?.join("-");
+
+  try {
+    const edgeRedirectsHasRedirectKey = await edgeConfigHas(redirectKey);
+
+    if (edgeRedirectsHasRedirectKey) {
+      const redirectApi = new URL(`/api/redirects/${redirectKey}`, request.nextUrl.origin);
+      const redirectData = await fetch(redirectApi);
+
+      const redirectEntry: RedirectsEntry | undefined = await redirectData?.json();
+      const statusCode = redirectEntry?.permanent ? 308 : 307;
+
+      return NextResponse.redirect(new URL(redirectEntry?.destination as string, request.nextUrl.origin), statusCode);
+    };
+
+    return NextResponse.next();
+  } catch (e) {
+    return NextResponse.json(e);
+  };
+};
+
+export const config = {
+  matcher: ["/(api/v1/revenues*.*)"],
+};
