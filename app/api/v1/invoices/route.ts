7a965823d0acf487067a889be24b0b8094cce119
Abdullah Numan <anewman15@hotmail.com>
2025-09-04 11:51:53 +0600

    feat: add geolocation based redirect to /v1/invoices

--- app/api/v1/invoices/route.ts
+++ app/api/v1/invoices/route.ts
@@ -1,12 +1,24 @@
+/* eslint-disable prefer-const */
 import { NextRequest, NextResponse } from "next/server";
 import { invoices } from "@/app/lib/strapi/strapiClient";
+import geoip from "geoip-country";
 
-export async function GET() {
+export async function GET(request: NextRequest) {
+  const ip = (await request.headers.get("x-forwarded-for"))?.split(",")[0];
+  let country = "FR" as string;
+  let allInvoices;
+
   try {
-    const allInvoices = await invoices.find();
-    return NextResponse.json(allInvoices);
+    allInvoices = await invoices.find();
+    // country = (await geoip.lookup(ip as string))?.country as string;
   } catch (e) {
+
     return NextResponse.json(e);
+
+  } finally {
+    if (country !== "FR") { return NextResponse.json(allInvoices) };
+
+    return NextResponse.redirect(new URL("/api/v2/invoices/fr", request.url), { status: 302 });
   };
 };
 
