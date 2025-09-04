27069b87e432738b146faf791da2128526a2dda4
Abdullah Numan <anewman15@hotmail.com>
2025-09-04 11:54:50 +0600

    feat: add route handler for redirect keys

---
+++ app/api/redirects/[path]/route.ts
@@ -0,0 +1,16 @@
+import { NextRequest, NextResponse } from "next/server";
+import { get } from "@vercel/edge-config";
+
+export async function GET(
+  request: NextRequest,
+  { params }: { params: Promise<{ path: string }>}
+) {
+  try {
+    const path = (await params)?.path;
+    const redirectEntry = await get(path);
+    
+    return NextResponse.json({ path: redirectEntry });
+  } catch (e) {
+    return NextResponse.json(e);
+  };
+};
